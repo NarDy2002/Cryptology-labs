5706e40469518b6b10415ce1cd12a356294ffe68
NarDy2002 <nardep2002@gmail.com>
2022-02-22 09:19:39 +0200

    Added option to load dict to file

--- frequency.py
+++ frequency.py
@@ -1,4 +1,4 @@
-
+import json
 
 class WordsDict(dict):
 
@@ -12,19 +12,26 @@ class WordsDict(dict):
             for line in f.readlines():
                 for word in line.strip().split():
                     for letter in word:
-                        self[letter] = self.get(letter, 0) + 1
-                        overall_length+=1
+                        if letter.isalpha:
+                            self[letter] = self.get(letter, 0) + 1
+                            overall_length+=1
         
         for letter in self.keys():
             self[letter] = self[letter] / overall_length
-        
+    
+    def write_to_file(self,filename:str) -> None:
+        if not isinstance(filename,str):
+            raise TypeError("Bad type of filename")
+
+        with open(filename, 'w' ,encoding="utf-8") as f:
+            json.dump(self, f, indent = 2) 
 
 
 def main():
     words = WordsDict()
     words.fill_from_file("words.txt")
     print(words)
-
+    words.write_to_file("frequency_dict.json")
     
 
 
