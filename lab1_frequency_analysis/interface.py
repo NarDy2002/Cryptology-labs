ab2e051e189b9b5ae45e17fb9b56e1bb4d147110
NarDy2002 <nardep2002@gmail.com>
2022-03-17 16:18:29 +0200

    Renamed file and fixed bug + Dictionary cleaned before filling + Added ability to add words from strings

--- lab1_frequency_analysis/interface.py
+++ lab1_frequency_analysis/interface.py
@@ -1,29 +1,35 @@
 import json
 
-class WordsDict(dict):
+class WordsDict(dict):  
 
     def fill_from_file(self, filename:str) -> None:
         if not isinstance(filename,str):
             raise TypeError("Bad type of filename")
 
-        overall_length = 0
-
         with open(filename, 'r',encoding="utf-8") as f:
-            for line in f.readlines():
-                for word in line.strip().split():
-                    for letter in word:
-                        if letter.isalpha:
-                            self[letter] = self.get(letter, 0) + 1
-                            overall_length+=1
-        
-        for letter in self.keys():
-            self[letter] = self[letter] / overall_length
+            text = f.read()
+            self.fill_from_str(text)
+
+
+    def fill_from_str(self, text:str) -> None:
     
+        self.clear()
+        overall_length = 0
+        
+        for word in text.split():
+            for letter in word:
+                if letter.isalpha:
+                    self[letter] = self.get(letter, 0) + 1
+                    overall_length+=1
+
+        for letter in self.keys():
+            self[letter] = self[letter] / overall_length   
+
     def write_to_file(self,filename:str) -> None:
         if not isinstance(filename,str):
             raise TypeError("Bad type of filename")
 
-        with open(filename, 'r' ,encoding="utf-8") as f:
+        with open(filename, 'w' ,encoding="utf-8") as f:
             json.dump(self, f, indent = 2) 
 
 
